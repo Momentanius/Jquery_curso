d46e2e7f30f15db20962a7de95bb2332fd158f39
Guilherme Gustavo Gohr <guilhermegustavogohr@gmail.com>
2017-12-12 13:22:51 -0200

    Jogo da velha

--- JQuery/jogo da velha/jogo.js
+++ JQuery/jogo da velha/jogo.js
@@ -1,9 +1,33 @@
-$(document).ready(function(){
+$(document).ready( function(){
 
-  alert('deu certo!');
+  $('#botao').click( function(){
+
+    if ($('#entrada_apelido_jogador_1').val()==''){
+
+      alert('Apelido do jogador 1 não foi preenchido');
+      return false;
+
+    }
+
+    if ($('#entrada_apelido_jogador_2').val()==''){
+
+      alert('Apelido do jogador 1 não foi preenchido');
+      return false;
+    }
 
 
-}){
+
+    $('#nome_jogador_1').html($('#entrada_apelido_jogador_1').val());
+    $('#nome_jogador_2').html($('#entrada_apelido_jogador_2').val());
 
 
-}
+    //Exibir grid
+
+    $('#pagina_inicial').hide();
+    $('#palco_jogo').show();
+
+
+  }) ;
+
+
+});
